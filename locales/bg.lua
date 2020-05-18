4850868164c639ecfd3e7c14120824a2fe803c26
Shaggy Rogers <62436188+ShaggyRogersTBS@users.noreply.github.com>
2020-05-18 16:04:31 +0300

    Create bg.lua

---
+++ locales/bg.lua
@@ -0,0 +1,31 @@
+Locales['bg'] = {
+  -- cloakroom
+  ['cloakroom_menu'] = 'Съблекалня',
+  ['cloakroom_prompt'] = 'Натиснете ~INPUT_CONTEXT~ , за да отворите ~y~Шкафчето~s~.',
+  ['wear_citizen'] = 'Цивилно Облекло',
+  ['wear_work'] = 'Общинско Облекло',
+
+  -- garage
+  ['spawner_prompt'] = 'Натиснете ~INPUT_CONTEXT~ , за да отворите ~y~Служебният гараж~s~.',
+  ['store_veh'] = 'Натиснете ~INPUT_CONTEXT~, за да приберете колата',
+  ['spawn_veh'] = 'Изкарайте кола',
+  ['spawnpoint_blocked'] = 'Има паркиран автомобил на мястото за изваждане!',
+  ['only_parlament'] = 'Можете да паркирате само служебни коли',
+
+  ['amount_invalid'] = 'Невалидна бройка',
+  ['press_to_open'] = 'Натиснете ~INPUT_CONTEXT~ , за да отворите менюто',
+  ['billing'] = 'Фактуриране',
+  ['billing_sent'] = 'Фактурата е подпечатана!',
+  ['invoice_amount'] = 'Стойност на фактурата',
+  ['no_players_near'] = 'Няма играчи наблизо',
+  ['deposit_stock'] = 'Депозиране на материали',
+  ['take_stock'] = 'Взимане на материали',
+  ['boss_actions'] = 'Шефско меню',
+  ['quantity'] = 'Количество',
+  ['quantity_invalid'] = 'Невалидно Количество!',
+  ['inventory'] = 'Инвентар',
+  ['parlament_client'] = 'Общински Клиент',
+  ['have_withdrawn'] = 'Изтеглихте ~y~x%s~s~ ~b~%s~s~',
+  ['have_deposited'] = 'Депозирахте ~y~x%s~s~ ~b~%s~s~',
+  ['player_cannot_hold'] = 'Вие~r~нямате~s~ достатъчно ~y~място~s~ в инвентара!',
+}
