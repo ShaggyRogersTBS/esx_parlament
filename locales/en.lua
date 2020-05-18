76e25631ba3dc565f76103d14f9897bd5235fb47
Shaggy Rogers <62436188+ShaggyRogersTBS@users.noreply.github.com>
2020-05-18 16:04:44 +0300

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,31 @@
+Locales['en'] = {
+  -- cloakroom
+  ['cloakroom_menu'] = 'cloakroom',
+  ['cloakroom_prompt'] = 'press ~INPUT_CONTEXT~ to access the ~y~Cloakroom~s~.',
+  ['wear_citizen'] = 'citizen wear',
+  ['wear_work'] = 'parlament wear',
+
+  -- garage
+  ['spawner_prompt'] = 'press ~INPUT_CONTEXT~ to access the ~y~Garage~s~.',
+  ['store_veh'] = 'press ~INPUT_CONTEXT~ to store the vehicle',
+  ['spawn_veh'] = 'spawn vehicle',
+  ['spawnpoint_blocked'] = 'there is a vehicle blocking the spawnpoint!',
+  ['only_parlament'] = 'you can only store taxis.',
+
+  ['amount_invalid'] = 'amount invalid',
+  ['press_to_open'] = 'press ~INPUT_CONTEXT~ to access the menu',
+  ['billing'] = 'billing',
+  ['billing_sent'] = 'the bill has been registered!',
+  ['invoice_amount'] = 'invoice amount',
+  ['no_players_near'] = 'no players nearby',
+  ['deposit_stock'] = 'deposit Stock',
+  ['take_stock'] = 'take Stock',
+  ['boss_actions'] = 'boss Actions',
+  ['quantity'] = 'quantity',
+  ['quantity_invalid'] = 'that is an invalid quantity!',
+  ['inventory'] = 'inventory',
+  ['parlament_client'] = 'taxi Client',
+  ['have_withdrawn'] = 'you have withdrawn ~y~x%s~s~ ~b~%s~s~',
+  ['have_deposited'] = 'you have deposited ~y~x%s~s~ ~b~%s~s~',
+  ['player_cannot_hold'] = 'you do ~r~not~s~ have enough ~y~free space~s~ in your inventory!',
+}
